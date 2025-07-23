02d7bab84c018991ca8c0e44ff6252a254d4fb8f
SaiKiran93814 <teppali123@gmail.com>
2025-07-23 14:40:31 +0530

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CartProvider>
+  <React.StrictMode>
     <App />
-  </CartProvider>
+  </React.StrictMode>
 );
