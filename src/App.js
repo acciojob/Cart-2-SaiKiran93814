8c951facf7bb37f843821c26d7ce1f644f28bc2c
SaiKiran93814 <teppali123@gmail.com>
2025-07-23 14:37:16 +0530

    Completed assignment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import CartList from './components/CartList';
+
+const App = () => {
+  return (
+    <main id="main">
+      <Navbar />
+      <CartList />
+    </main>
+  );
+};
+
+export default App;
